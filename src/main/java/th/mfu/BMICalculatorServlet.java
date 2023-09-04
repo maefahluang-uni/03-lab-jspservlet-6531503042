11dfcd98e9eaf450db56af5096f817a3040833d8
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2023-09-04 16:18:02 +0700

    gg

--- src/main/java/th/mfu/BMICalculatorServlet.java
+++ src/main/java/th/mfu/BMICalculatorServlet.java
@@ -1,27 +1,24 @@
 package th.mfu;
 
 import java.io.IOException;
-
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-//TODO: add webservlet to "/calbmi"
 @WebServlet("/calbmi")
-public class BMICalculatorServlet extends HttpServlet{
+public class BMICalculatorServlet extends HttpServlet {
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO: get parameter from request: "weight" and "height
-        String wString = request.getParameter("weight");
-        String wHeight = request.getParameter("height");
+        String weightStr = request.getParameter("weight");
+        String heightStr = request.getParameter("height");
 
-        if (wString != null && !wString.isEmpty() && wHeight != null && !wHeight.isEmpty()) {
+        if (weightStr != null && !weightStr.isEmpty() && heightStr != null && !heightStr.isEmpty()) {
             try {
-                double weight = Double.parseDouble(request.getParameter("Weight"));
-                double height = Double.parseDouble(request.getParameter("Height"));
+                double weight = Double.parseDouble(weightStr);
+                double height = Double.parseDouble(heightStr);
 
                 double bmi = calculateBMI(weight, height);
                 String build = determineBuild(bmi);
@@ -39,19 +36,20 @@ public class BMICalculatorServlet extends HttpServlet{
     }
 
     private double calculateBMI(double weight, double height) {
-        return weight / (height) * (height);
+        return weight / ((height) * (height));
     }
 
     private String determineBuild(double bmi) {
-
         if (bmi < 18.5) {
-            return ("underweight");
-        } else if (bmi >= 18.5 && bmi < 25) {
-            return ("normal");
-        } else if (bmi <= 25 && bmi < 30) {
-            return ("obese");
+            return "underweight";
+        } else if (bmi < 25) {
+            return "normal weight";
+        } else if (bmi < 30) {
+            return "overweight";
+        } else if (bmi < 35) {
+            return "obese";
         } else {
-            return ("extremely obese");
+            return "extremely obese";
         }
     }
 }
